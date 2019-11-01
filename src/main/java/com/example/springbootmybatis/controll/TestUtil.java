a0e4fa227ea989cd4177b3bb1c45dd7d54827005
wangli <1008611l>
2019-11-01 11:30:05 +0800

    提交代码-mybatis 逆向工程

--- src/main/java/com/example/springbootmybatis/controll/TestUtil.java
+++ src/main/java/com/example/springbootmybatis/controll/TestUtil.java
@@ -1,11 +1,23 @@
 package com.example.springbootmybatis.controll;
 
 
+import com.example.springbootmybatis.util.RedisUtil;
 
-
-
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class TestUtil {
+    @Autowired
+    RedisUtil redisUtil;
+
+
+
+    public void test(){
+        if(redisUtil.hasKey("name")){
+            System.err.println(1);
+        }else{
+            System.err.println("err");
+        }
+    }
 
 //    public static void main(String[] args) {
 //        String[] b = {"1","2","3"};
